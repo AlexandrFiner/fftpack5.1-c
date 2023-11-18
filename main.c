af74bc72e0b2bc2e44acf17d36ab98eb70d975fe
AlexandrFiner <finersanya@gmail.com>
2023-11-18 16:50:05 +0500

    test cfftmi

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define lensav(n) (2*n + int(log2(n)) + 6)
+
 int main(int argc, char* argv[]) {
-    c1f3kb_();
-    printf("Hello world\n");
+
+    int n = 1;
+    float wsave = 10;
+    int ier;
+
+    cfftmi_(n, wsave, lensav(n), ier);
+    printf("Hello world %d\n", ier);
     return EXIT_SUCCESS;
 }
