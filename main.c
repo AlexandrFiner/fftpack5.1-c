81a609b91d6e7409c18796884bc711ba1549b65c
Alexandr Morohin <finersanya@gmail.com>
2023-12-23 16:25:26 +0500

    fix

--- main.c
+++ main.c
@@ -4,24 +4,13 @@
 #include "f2c.h"
 
 #define lensav(n) (2*n + (int)(log2(n)) + 6)
-//
-//int main(int argc, char* argv[]) {
-//
-//    int n = 1;
-//    float wsave = 10;
-//    int ier;
-//
-//    cfftmi_(n, wsave, lensav(n), ier);
-//    printf("Hello world %d\n", ier);
-//    return EXIT_SUCCESS;
-//}
 
 int cfftmi_(int *n, float *wsave, int *lensav, int *ier);
 
 int main(int argc, char* argv[]) {
 
-    int n = 2;
-    float wsave = 0.1;
+    int n = 2;          // Длина сигнала
+    float wsave = 0.1;  // Частота
     int ier = 5;
     int lensv = lensav(n);
 
