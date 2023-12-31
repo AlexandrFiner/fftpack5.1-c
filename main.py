5e484f781ef6018c18ced2a356ca6cdb2ca71a41
AlexandrViktorovich <kent1234810@gmail.com>
2023-12-31 20:54:13 +0500

    fix

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import struct
 # Sample arrays of floats
 float_array1 = [1.23, 4.56, 7.89, 10.11]
 float_array2 = [2.34, 5.67, 8.90, 12.34]
+float_array3 = [8.90, 12.34,7.89, 10.11]
 
-# Pack floats from both arrays into binary data
-binary_data = struct.pack('f' * len(float_array1), *float_array1) + struct.pack('f' * len(float_array2), *float_array2)
 
-# Specify the file path
-file_path = 'float_data.bin'
 
-# Write binary data to file
-with open(file_path, 'wb') as file:
-    file.write(binary_data)
+array = [float_array1,float_array2,float_array3]
+
+for arr in range(len(array)):
+    b_data = struct.pack('f'* len(array[arr]),*array[arr])
+    file_path = f'float_data{arr}.bin'
+    with open("tests/"+file_path, 'wb') as file:
+        file.write(b_data)
+    print(f"file {arr} was created successfully")
+
+
+
+
+
+
